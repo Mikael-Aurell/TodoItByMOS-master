c4b402255e1413f436b8869fb5065a44b8f35d48
Mikael Aurell <aurell.mikael@gmail.com>
2021-02-18 18:08:00 +0100

    Added Interface TodoItems

--- src/main/java/se/lexicon/data/TodoItems.java
+++ src/main/java/se/lexicon/data/TodoItems.java
@@ -1,5 +1,18 @@
 package se.lexicon.data;
 
-public interface TodoItems {
+import se.lexicon.model.Person;
+import se.lexicon.model.Todo_Item;
 
+import java.util.List;
+
+public interface TodoItems {
+Todo_Item create(Todo_Item todo_item);
+List<Todo_Item> findAll();
+Todo_Item findById(int id);
+List<Todo_Item> findByDoneStatus(boolean done);
+List<Todo_Item> findByAssignee(int assignee);
+List<Todo_Item> findByAssignee(Person person);
+List<Todo_Item> findByUnassignedTodoItems();
+Todo_Item update(Todo_Item todo_item);
+boolean deleteById(int id);
 }
